b5f4a321ba90de5eed162480f0767bc4d1412442
Elm3refaMecca <Elm3refaMecca@users.noreply.github.com>
2025-11-23 22:11:27 +0000

    deploy: 7447528f13feabb4d87d31b4e05c0eeca8e3a75f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2633581858"
+    serviceWorkerVersion: "1331886706"
   }
 });
