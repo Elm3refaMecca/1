4f3c5cb0a5fdd89b5bf6d578e905a6b6afdbdd34
Elm3refaMecca <Elm3refaMecca@users.noreply.github.com>
2026-09-14 20:21:38 +0000

    deploy: e66571886c6182bcbe59701b349298fdaec686fd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2800011944" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3299436506" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
